3c3762df9c1d0d9184b4fb2b286cf6cca04d964e
Matkovic123 <toni.matkovic0@gmail.com>
2017-03-05 23:11:03 +0100

    Initial commit - empty project

---
+++ src/main/java/feeddit/FeedditCloudAppApplication.java
@@ -0,0 +1,12 @@
+package feeddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedditCloudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedditCloudAppApplication.class, args);
+	}
+}
